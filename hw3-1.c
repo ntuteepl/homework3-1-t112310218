1ca927fc96014325d6b94a3935bacfa75fe61f2f
t112310218 <146065077+t112310218@users.noreply.github.com>
2023-10-21 15:05:44 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void printString(char String[], bool TurnLine){
+    for(int i; i < strlen(String); i ++){
+        printf("%c", String[i]);
+    }
+    if(TurnLine) printf("\n");
+}
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    for(int i = 2; i < a ;i++ ){
+        if(a % i == 0){
+            printString("NO", true);
+            return 0;
+        }
+    }
+
+    printString("YES", true);
+    return 0;
+}
+
