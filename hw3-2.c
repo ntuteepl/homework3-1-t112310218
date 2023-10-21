9c7da3253c1e1dac99685ac1fcbcf16de28dd615
t112310218 <146065077+t112310218@users.noreply.github.com>
2023-10-21 15:17:08 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void printString(char String[], bool TurnLine){
+    for(int i; i < strlen(String); i ++){
+        printf("%c", String[i]);
+    }
+    if(TurnLine) printf("\n");
+}
+
 int main()
 {
     int s1, d1, s2, d2, s3, d3;
@@ -9,10 +16,30 @@ int main()
     scanf("%d%d%d%d%d%d", &s1, &d1,&s2, &d2, &s3, &d3);
 
     for(int i = 1; i < 25 ; i++){
-        if(i == d1 || i == d2 || i == d3){
+        if(i == d1){
             n_use += 1;
         }
-        if(i == s1 || i == s2 || i == s3){
+        if(i == d2){
+            n_use += 1;
+        }
+        if(i == d3){
+            n_use += 1;
+        }
+        if(i == s1){
+            if(n_use == 0){
+                count += 1;
+            }else{
+                n_use -= 1;
+            }
+        }
+        if(i == s2){
+            if(n_use == 0){
+                count += 1;
+            }else{
+                n_use -= 1;
+            }
+        }
+        if(i == s3){
             if(n_use == 0){
                 count += 1;
             }else{
